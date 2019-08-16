f5be170d2bc587fe23fae524bc68ffe39b89fdd4
YukoChida <chida.yuko@vareal.co.jp>
2019-08-16 19:15:02 +0900

    Bookの表示と新規作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root 'books#index'
+  get 'books/new', to:'books#new'
+  post 'books/new', to:'books#create'
+  get 'books/show/:id', to:'books#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#hello'
+
 end
